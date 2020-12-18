1fd7d4d456b3dcac9c83007f4d888aef2441fef2
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-12-18 10:05:59 -0500

    added sidebar

--- src/components/ClosingsPage.jsx
+++ src/components/ClosingsPage.jsx
@@ -4,6 +4,7 @@ import PrimaryDepartmentClosing from "./PrimaryDepartmentClosing";
 import ClosingsTable from "./ClosingTable";
 import ClosingHeaders from "./ClosingHeaders";
 import ClosingRows from "./ClosingRows";
+import ClosingsSideBar from "./ClosingsSideBar";
 
 const ClosingsPage = (props) => {
   const [{ closings = [], hasError, isLoading }] = useClosings();
@@ -24,19 +25,27 @@ const ClosingsPage = (props) => {
   }
 
   return (
-    <div>
-      {" "}
-      {isLoading ? (
-        <p>{`Loading Closing Information...`}</p>
-      ) : (
-        <div>
-          <PrimaryDepartmentClosing data={records}/>
-          <ClosingsTable id="responsive-main-table" className="display">
-            <ClosingHeaders />
-            <ClosingRows data={records} />
-          </ClosingsTable>
+    <div className="dg_internal-template">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 col-sm-12">
+            {isLoading ? (
+              <p>{`Loading Closing Information...`}</p>
+            ) : (
+              <div id="dg_main-content">
+                <PrimaryDepartmentClosing data={records} />
+                <ClosingsTable id="responsive-main-table" className="display">
+                  <ClosingHeaders />
+                  <ClosingRows data={records} />
+                </ClosingsTable>
+              </div>
+            )}
+          </div>
+          <div className="col-md-4 col-sm-12">
+            <ClosingsSideBar />
+          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
